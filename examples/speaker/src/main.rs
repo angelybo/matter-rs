1dd3ed146de7f9172115d63c3731e7dd97b2f3d9
Angel Yankov <angy@bang-olufsen.dk>
2023-02-22 15:34:44 +0200

    Fix speaker example

--- examples/speaker/src/main.rs
+++ examples/speaker/src/main.rs
@@ -18,9 +18,38 @@
 mod dev_att;
 use matter::core::{self, CommissioningData};
 use matter::data_model::cluster_basic_information::BasicInfoConfig;
-use matter::data_model::cluster_media_playback::{Commands, MediaPlaybackCluster};
+use matter::data_model::cluster_level_control::{LevelControlCluster, Commands as LvlCommands} ;
+use matter::data_model::cluster_media_playback::{MediaPlaybackCluster, Commands as MediaCommands};
 use matter::data_model::device_types::DEV_TYPE_ON_SMART_SPEAKER;
 use matter::secure_channel::spake2p::VerifierData;
+use log::{info,debug, error};
+
+fn setup_media_playback_callbacks(media_playback_cluster: &mut Box<MediaPlaybackCluster>) {
+    let play_callback = Box::new(|| info!("Comamnd [Play] handled with callback."));
+    let pause_callback = Box::new(|| info!("Comamnd [Pause] handled with callback."));
+    let stop_callback = Box::new(|| info!("Comamnd [Stop] handled with callback."));
+    let start_over_callback =
+        Box::new(|| info!("Comamnd [StartOver] handled with callback."));
+
+
+    media_playback_cluster.add_callback(MediaCommands::Play, play_callback);
+    media_playback_cluster.add_callback(MediaCommands::Pause, pause_callback);
+    media_playback_cluster.add_callback(MediaCommands::Stop, stop_callback);
+    media_playback_cluster.add_callback(MediaCommands::StartOver, start_over_callback);
+}
+
+fn setup_level_control_callbacks(level_control_cluster: &mut Box<LevelControlCluster>) {
+    let move_to_lvl_callback = Box::new(|a,b,c| info!("Command [MoveToLevel] handled."));
+    let move_callback = Box::new(|_,_,_| info!("Command [Move] handled."));
+    let step_callback = Box::new(|_,_,_| info!("Command [Step] handled."));
+    let stop_callback = Box::new(|_,_,_| info!("Command [Stop] handled."));
+
+    level_control_cluster.add_data_callback(LvlCommands::MoveToLevel, move_to_lvl_callback);
+    level_control_cluster.add_data_callback(LvlCommands::Move, move_callback);
+    level_control_cluster.add_data_callback(LvlCommands::Step, step_callback);
+    level_control_cluster.add_data_callback(LvlCommands::Stop, stop_callback);
+}
+
 
 fn main() {
     env_logger::init();
@@ -48,22 +77,42 @@ fn main() {
         let mut node = dm.node.write().unwrap();
 
         let endpoint_audio = node.add_endpoint(DEV_TYPE_ON_SMART_SPEAKER).unwrap();
-        let mut media_playback_cluster = MediaPlaybackCluster::new().unwrap();
+        let mut lvl_control_cluster = match LevelControlCluster::new() {
+            Ok(t) => t,
+            Err(_) => {
+                error!("Failed to allocate LevelControlCluster");
+                panic!("Failed to allocate MediaPlaybackCluster");
+            },
+        };
+        let mut media_playback_cluster = match MediaPlaybackCluster::new() {
+            Ok(t) => t,
+            Err(_) => {
+                error!("Failed to allocate MediaPlaybackCluster");
+                panic!("Failed to allocate MediaPlaybackCluster");
+            }
+        };
 
-        // Add some callbacks
-        let play_callback = Box::new(|| log::info!("Comamnd [Play] handled with callback."));
-        let pause_callback = Box::new(|| log::info!("Comamnd [Pause] handled with callback."));
-        let stop_callback = Box::new(|| log::info!("Comamnd [Stop] handled with callback."));
-        let start_over_callback =
-            Box::new(|| log::info!("Comamnd [StartOver] handled with callback."));
-        media_playback_cluster.add_callback(Commands::Play, play_callback);
-        media_playback_cluster.add_callback(Commands::Pause, pause_callback);
-        media_playback_cluster.add_callback(Commands::Stop, stop_callback);
-        media_playback_cluster.add_callback(Commands::StartOver, start_over_callback);
+        setup_media_playback_callbacks(&mut media_playback_cluster);
+        setup_level_control_callbacks(&mut lvl_control_cluster);
 
-        node.add_cluster(endpoint_audio, media_playback_cluster)
-            .unwrap();
-        println!("Added Speaker type at endpoint id: {}", endpoint_audio)
+        match node.add_cluster(endpoint_audio, lvl_control_cluster) {
+            Ok(t) =>  {
+                debug!("Added level control cluster to node");
+            },
+            Err(_) => {
+                error!("Failed to add cluster to node: Too many clusters");
+            }
+        };
+        match node.add_cluster(endpoint_audio, media_playback_cluster) {
+            Ok(t) => {
+                debug!("Added level control cluster to node");
+            },
+            Err(_) => {
+                error!("Failed to add cluster to node: Too many clusters");
+            }
+        };
+
+        println!("Added Speaker type at endpoint id: {}", endpoint_audio);
     }
     matter.start_daemon().unwrap();
 }
